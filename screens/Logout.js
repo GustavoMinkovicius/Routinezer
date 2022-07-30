82790cc982ceb9190080e94f53d529d2eda51e67
GustavoMinkovicius <87321967+GustavoMinkovicius@users.noreply.github.com>
2022-07-29 22:50:22 -0300

    Add files via upload

--- screens/Logout.js
+++ screens/Logout.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import firebase from 'firebase'
+import {getData} from '../services/file.js'
+import {getTasks} from  '../services/tasks'
 
 export default class Logout extends Component {
     componentDidMount(){
-        firebase.default.auth().signOut()
+      var id = getData("user_id")
+      getTasks(id)
+        firebase.default.auth().signOut().then(function() {
+          console.log('Signed Out');
+        }, function(error) {
+          console.error('Sign Out Error', error);
+        });
     }
   render() {
     return (
